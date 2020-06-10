68b14755f66c7be2a32fe310615e47741639c280
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-10 17:22:09 -0500

    completed name and age portion of chatbot

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,18 +6,23 @@ public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2018.");
+        System.out.println("Hello! My name is CatChat.");
+        System.out.println("I was created in 2020.");
         System.out.println("Please, remind me your name.");
+        String yourName = scanner.next();
 
         // reading a name
 
-        System.out.println("What a great name you have, {yourName}!");
+        System.out.println("What a great name you have, " + yourName + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int remainder3 = scanner.nextInt(); //2
+        int remainder5 = scanner.nextInt(); //2
+        int remainder7 = scanner.nextInt(); //4
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
         // reading all remainders
 
-        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
