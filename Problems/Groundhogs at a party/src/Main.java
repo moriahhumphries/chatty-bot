fd582f7dd5a3d9d3fa3142482e18ebc3ac2252ef
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-10 18:43:01 -0500

    completed relational operator problems

---
+++ Problems/Groundhogs at a party/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int cups = scanner.nextInt();
+        boolean weekend = scanner.nextBoolean();
+
+        System.out.println(((cups >= 10 && cups <= 20) && !weekend) || (cups >= 15 && cups <= 25) && weekend);
+
+    }
+}
