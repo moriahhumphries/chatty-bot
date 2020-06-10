3843b356eec290e47c01605b6a31fe564699dc40
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-10 11:42:01 -0500

    completed guest list challenge

---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,26 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String guest1 = scanner.next(); //"Ann";
+        String guest2 = scanner.next(); //"Boris";
+        String guest3 = scanner.next(); //"Paul";
+        String guest4 = scanner.next(); //"Susan";
+        String guest5 = scanner.next(); //"Mary";
+        String guest6 = scanner.next(); //"John";
+        String guest7 = scanner.next(); //"Kate";
+        String guest8 = scanner.next(); //"Jane";
+        System.out.println(guest8);
+        System.out.println(guest7);
+        System.out.println(guest6);
+        System.out.println(guest5);
+        System.out.println(guest4);
+        System.out.println(guest3);
+        System.out.println(guest2);
+        System.out.println(guest1);
+    }
+}
