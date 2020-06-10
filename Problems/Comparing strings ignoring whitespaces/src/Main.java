3ac9b5aea1569d11fcea5e1c1d42dae7118e717f
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-10 17:05:47 -0500

    completed additional problems

---
+++ Problems/Comparing strings ignoring whitespaces/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+
+        String str2 = scanner.nextLine();
+        System.out.println(str1.replace(" ", "").equals(str2.replace(" ", "")));
+    }
+}
