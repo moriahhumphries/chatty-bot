d854c766dc90247e72177ab40e0d81f2888766c4
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-09 22:25:02 -0500

    started chat bot

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,8 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is CatBot");
+        System.out.println("I was created in 2020");
+    }
+}
