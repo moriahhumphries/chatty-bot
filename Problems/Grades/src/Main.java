d9322aea3280ddac24440b05734514196fef996d
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-11 23:05:49 -0500

    completed additional questions

---
+++ Problems/Grades/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        // 5 is for A
+        // 4 is for B
+        // 3 is for C
+        // 2 is for D
+        Scanner scanner = new Scanner(System.in);
+        int A = 5;
+        int B = 4;
+        int C = 3;
+        int D = 2;
+        int n;
+        for(n = 1; n <= 13; n++){
+            System.out.println(n);
+        }
+    }
+
+}
